7af71d72bee4a0d46b5317b6681172eb322fe543
Madeleine <madeleine.schonemann@gmail.com>
2021-04-21 13:00:11 +0200

    Draft for video

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import styles from "./hero.module.scss";
+import video from "./hero.mp4";
 
 const Hero = ({ children }) => (
   <header className={styles.masthead} id="start">
+    <video
+      playsInline
+      autoPlay
+      muted
+      loop
+      poster="polina.jpg"
+      className={styles.video}
+    >
+      <source src={video} type="video/mp4" />
+    </video>
     {children}
   </header>
 );
