675ff4d91a4fbc0179493ecfc7369f4ec114f892
Madeleine <madeleine.schonemann@gmail.com>
2021-04-28 19:04:06 +0200

    Fallback to image when video is suspended

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./hero.module.scss";
 import video from "./hero.mp4";
+import heroImage from "./hero.jpg";
 
-const Hero = ({ children }) => (
-  <header className={styles.masthead} id="start">
-    <video
-      playsInline
-      autoPlay
-      muted
-      loop
-      poster="polina.jpg"
-      className={styles.video}
-    >
-      <source src={video} type="video/mp4" />
-    </video>
-    <div className={styles.content}>{children}</div>
-  </header>
-);
+const Hero = ({ children }) => {
+  const videoElement = useRef(null);
+
+  const handleSuspend = () => {
+    videoElement.current.style.visibility = "hidden";
+  };
+
+  const handlePlay = () => {
+    videoElement.current.style.visibility = "visible";
+  };
+
+  return (
+    <header className={styles.masthead} id="start">
+      <div className={styles.videoContainer}>
+        <video
+          playsInline
+          autoPlay
+          muted
+          loop
+          poster={heroImage}
+          className={styles.video}
+          onSuspend={handleSuspend}
+          onPlay={handlePlay}
+          ref={videoElement}
+        >
+          <source src={video} type="video/mp4" />
+        </video>
+      </div>
+      <div className={styles.content}>{children}</div>
+    </header>
+  );
+};
 
 export default Hero;
