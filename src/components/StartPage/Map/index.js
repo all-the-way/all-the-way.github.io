76e1b1cec78ba8b5083aba259ab7cba2e031e076
Madeleine <madeleine.schonemann@gmail.com>
2021-10-03 18:24:25 +0200

    Add opening hours

--- src/components/StartPage/Map/index.js
+++ src/components/StartPage/Map/index.js
@@ -8,8 +8,18 @@ const Map = () => (
       <MapContainer></MapContainer>
     </div>
     <div className={styles.address}>
-      <p>Djäknegatan 33</p>
-      <p>211 35 Malmö</p>
+      <div>
+        <p>Djäknegatan 33</p>
+        <p>211 35 Malmö</p>
+      </div>
+      <div className={styles.openingHours}>
+        <h4 className="text-white">Öppettider</h4>
+        Öppet varje dag kl 05:00-23:00
+        <h4 className="text-white">Bemannade tider:</h4>
+        <p>Måndag-torsdag: 08:00-20:00</p>
+        <p>Fredag: 08:00-18:00</p>
+        <p>Lördag-söndag: 09:00-17:00</p>
+      </div>
     </div>
   </section>
 );
